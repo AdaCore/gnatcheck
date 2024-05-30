AdaCore/langkit-query-language

Replace all rules table with a hash map
Also replace `Rule_Id` with `Thin_Symbol` from
`Langkit_Support.Symbols`.

lkql_checker/src/gnatcheck-ids.ads
@@ -23,27 +23,72 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
---  This package defines ID types for rule table and category table. We define
+--  This package defines ID types for rule map and category table. We define
 --  ID types in a separate package because both rule type and category type
 --  needs both kinds of IDs
 
+with Ada.Containers;
+with Ada.Containers.Indefinite_Vectors;
+with Langkit_Support.Symbols; use Langkit_Support.Symbols;
+with Langkit_Support.Text;    use Langkit_Support.Text;
+
 package Gnatcheck.Ids is
 
-   -------------
-   -- Rule ID --
-   -------------
+   -----------------------------------
+   -- Internal rule id symbol table --
+   -----------------------------------
 
-   type Rule_Id is new Natural;
-   No_Rule    : constant Rule_Id := Rule_Id'First;
+   All_Rule_Ids : constant Langkit_Support.Symbols.Symbol_Table :=
+     Create_Symbol_Table;
+   --  This table contains all created rule identifiers
 
-   --  Fake Ids for compiler checks:
-   Restrictions_Id : constant Rule_Id := No_Rule + 1;
-   Style_Checks_Id : constant Rule_Id := Restrictions_Id + 1;
-   Warnings_Id     : constant Rule_Id := Style_Checks_Id + 1;
+   ------------------
+   -- Rule id type --
+   ------------------
 
-   subtype Compiler_Checks is Rule_Id range Restrictions_Id ..  Warnings_Id;
+   type Rule_Id is new Thin_Symbol;
+   --  Define the rule identifier as a thin symbol. The symbol belongs to
+   --  an internal symbol table, which contains all created rule ids. A rule
+   --  identifier represents the normalizd name of the rule.
 
-   First_Compiler_Check : constant := Restrictions_Id;
-   First_Rule           : constant Rule_Id := Warnings_Id + 1;
+   package Rule_Id_Vec is new Ada.Containers.Indefinite_Vectors
+     (Index_Type => Natural, Element_Type => Rule_Id);
+   --  A simple vector to store rule identifiers
+
+   ---------------------------
+   -- Operation on rule ids --
+   ---------------------------
+
+   function Find_Rule_Id (Rule_Name : String) return Rule_Id;
+   --  This function maps the `Find` function of `Langkit_Support.Symbols`
+   --  to the `Rule_Id` type, allowing user to get or create a rule identifier
+   --  from a simple string. Note that the input string is normalized before
+   --  the getting or creation of the rule identifier.
+
+   function Get_Id_Text (Id : Rule_Id) return String;
+   --  Get the text associated with the given `Rule_Id`, this text represents
+   --  the normalized name of the rule identified by `Id`.
+
+   function Is_Compiler_Check (Id : Rule_Id) return Boolean;
+   --  Get whether the given rule identifier represents a compiler check
+
+   function Hash (Id : Rule_Id) return Ada.Containers.Hash_Type is
+     (Hash (Get_Symbol (All_Rule_Ids, Thin_Symbol (Id))));
+   --  Shortcut fonction to hash a rule identifier
+
+   ------------------
+   -- Constant ids --
+   ------------------
+
+   No_Rule_Id : constant Rule_Id := Find (All_Rule_Ids, To_Text (""));
+   --  The empty identifier, used to express "no rule"
+
+   Restrictions_Id : constant Rule_Id :=
+     Find (All_Rule_Ids, To_Text ("restrictions"));
+   Style_Checks_Id : constant Rule_Id :=
+     Find (All_Rule_Ids, To_Text ("style_checks"));
+   Warnings_Id : constant Rule_Id :=
+     Find (All_Rule_Ids, To_Text ("warnings"));
+   --  Fake identifiers, used to represent the compiler checks
 
 end Gnatcheck.Ids;

AdaCore/gnatcheck

lkql_checker/src/gnatcheck-ids.ads
@@ -3,27 +3,70 @@
 --  SPDX-License-Identifier: GPL-3.0-or-later
 --
 
---  This package defines ID types for rule table and category table. We define
---  ID types in a separate package because both rule type and category type
---  needs both kinds of IDs
+--  This package defines ID type for rule map.
+
+with Ada.Containers;
+with Ada.Containers.Indefinite_Vectors;
+with Langkit_Support.Symbols; use Langkit_Support.Symbols;
+with Langkit_Support.Text;    use Langkit_Support.Text;
 
 package Gnatcheck.Ids is
 
-   -------------
-   -- Rule ID --
-   -------------
+   -----------------------------------
+   -- Internal rule id symbol table --
+   -----------------------------------
 
-   type Rule_Id is new Natural;
-   No_Rule    : constant Rule_Id := Rule_Id'First;
+   All_Rule_Ids : constant Langkit_Support.Symbols.Symbol_Table :=
+     Create_Symbol_Table;
+   --  This table contains all created rule identifiers
 
-   --  Fake Ids for compiler checks:
-   Restrictions_Id : constant Rule_Id := No_Rule + 1;
-   Style_Checks_Id : constant Rule_Id := Restrictions_Id + 1;
-   Warnings_Id     : constant Rule_Id := Style_Checks_Id + 1;
+   ------------------
+   -- Rule id type --
+   ------------------
 
-   subtype Compiler_Checks is Rule_Id range Restrictions_Id ..  Warnings_Id;
+   type Rule_Id is new Thin_Symbol;
+   --  Define the rule identifier as a thin symbol. The symbol belongs to
+   --  an internal symbol table, which contains all created rule ids. A rule
+   --  identifier represents the normalizd name of the rule.
 
-   First_Compiler_Check : constant := Restrictions_Id;
-   First_Rule           : constant Rule_Id := Warnings_Id + 1;
+   package Rule_Id_Vec is new Ada.Containers.Indefinite_Vectors
+     (Index_Type => Natural, Element_Type => Rule_Id);
+   --  A simple vector to store rule identifiers
+
+   ---------------------------
+   -- Operation on rule ids --
+   ---------------------------
+
+   function Find_Rule_Id (Rule_Name : String) return Rule_Id;
+   --  This function maps the `Find` function of `Langkit_Support.Symbols`
+   --  to the `Rule_Id` type, allowing user to get or create a rule identifier
+   --  from a simple string. Note that the input string is normalized before
+   --  the getting or creation of the rule identifier.
+
+   function Get_Id_Text (Id : Rule_Id) return String;
+   --  Get the text associated with the given `Rule_Id`, this text represents
+   --  the normalized name of the rule identified by `Id`.
+
+   function Is_Compiler_Check (Id : Rule_Id) return Boolean;
+   --  Get whether the given rule identifier represents a compiler check
+
+   function Hash (Id : Rule_Id) return Ada.Containers.Hash_Type is
+     (Hash (To_Symbol (All_Rule_Ids, Thin_Symbol (Id))));
+   --  Shortcut fonction to hash a rule identifier
+
+   ------------------
+   -- Constant ids --
+   ------------------
+
+   No_Rule_Id : constant Rule_Id := Find (All_Rule_Ids, To_Text (""));
+   --  The empty identifier, used to express "no rule"
+
+   Restrictions_Id : constant Rule_Id :=
+     Find (All_Rule_Ids, To_Text ("restrictions"));
+   Style_Checks_Id : constant Rule_Id :=
+     Find (All_Rule_Ids, To_Text ("style_checks"));
+   Warnings_Id : constant Rule_Id :=
+     Find (All_Rule_Ids, To_Text ("warnings"));
+   --  Fake identifiers, used to represent the compiler checks
 
 end Gnatcheck.Ids;