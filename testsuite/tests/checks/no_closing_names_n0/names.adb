AdaCore/langkit-query-language

Fix the 'No_Closing_Names' rule

testsuite/tests/checks/no_closing_names_n0/names.adb
@@ -1,0 +1,8 @@
+procedure Names is  --  FLAG
+   task T3;         --  FLAG
+
+   task body T3 is  --  NOFLAG
+   begin
+      null;
+   end T3;
+end;

AdaCore/gnatcheck

testsuite/tests/checks/no_closing_names_n0/names.adb
@@ -1,0 +1,8 @@
+procedure Names is  --  FLAG
+   task T3;         --  NOFLAG
+
+   task body T3 is  --  NOFLAG
+   begin
+      null;
+   end T3;
+end;