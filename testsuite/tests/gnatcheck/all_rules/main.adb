e1851e26fd49d3780b87004157c6f6184552cf16
Hugo Guerrier <guerrier@adacore.com>
2026-08-26 12:20:34 +0200

    minor: Fix 'gnatcheck/all_rules' test case

--- testsuite/tests/gnatcheck/all_rules/main.adb
+++ testsuite/tests/gnatcheck/all_rules/main.adb
@@ -2,8 +2,8 @@ with Ada.Text_IO; use Ada.Text_IO;  --  FLAG (3)
 
 procedure Main is                   --  FLAG (3)
 begin
-    Put_Line ("Hello!");
-    goto lbl                        --  FLAG (2)
+   Put_Line ("Hello!");
+   goto lbl;                        --  FLAG (2)
 
-    <<lbl>>
+   <<lbl>>
 end Main;
